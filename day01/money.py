43c04c47e1f621f3e392e282b3f67313e9b46d46
mizm <didehgur1@naver.com>
2018-12-18 10:31:53 +0900

    test commit

--- day01/money.py
+++ day01/money.py
@@ -4,6 +4,7 @@ import bs4
 doller = requests.get('https://finance.naver.com/marketindex/').text
 dsoup = bs4.BeautifulSoup(doller, 'html.parser')
 dolresult = dsoup.select_one('#exchangeList > li.on > a.head.usd > div > span.value').text
-
 print('지금 1원/달러 환율은 {0}원 입니다'.format(dolresult))
 print(f'지금 원/달러 환율은 {dolresult} 입니다.')
+
+
