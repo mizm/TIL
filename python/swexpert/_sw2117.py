f54a4ecffca6c736ae58d74e31ec68de3f4e71a2
miz <didehgur1@naver.com>
2019-01-21 17:36:12 +0900

    D13 | 190121 PM | swexpert

---
+++ python/swexpert/_sw2117.py
@@ -0,0 +1,30 @@
+def dfs(x,y,k) :
+    global n,m,home,result
+    dfsresult = 0
+    dfsmoney = 0
+    dfsmoney = k*k+(k-1)*(k-1)
+    for i,j in home :
+        if abs(x-i) + abs(y-j) < k :
+            dfsresult += 1
+    if dfsmoney <= dfsresult*m :
+        if result < dfsresult :
+            result = dfsresult
+    if k <= n :
+        dfs(x,y,k+1)
+    
+
+    
+for a in range(int(input())) :
+    n,m = list(map(int,input().split()))
+    item = [list(map(int,input().split())) for i in range(n)]
+    result = 0
+    home = []
+    for j, y in enumerate(item) :
+        for i, x in enumerate(y) :
+            if item[j][i] > 0 :
+                home.append((i,j))
+    for i in range(n) :
+        for j in range(n) :
+            dfs(i,j,1)
+    print(f'#{a+1} {result}')
+
--- python/swexpert/_sw5653.py
+++ python/swexpert/_sw5653.py
@@ -1,6 +1,40 @@
 for a in range(int(input())) :
     n,m,k = list(map(int,input().split()))
     item = [list(map(int,input().split())) for i in range(n)]
-    allmap = [[0 for i in range(1001)] for i in range 1001]
-    for i in  :
-        j 
+    my_map = {}
+    result = 0
+    time = 0
+    for i,l in enumerate(item) :
+        for j,t in enumerate(l) :
+            if item[i][j] > 0 :
+                my_map[(j,i)] =(item[i][j],item[i][j]*2)
+    
+    while time < k :
+        tempdic = {}
+        #print(time,len(my_map))
+        for key,value in my_map.items() :
+            if value[0] > 0:
+                if value[0] == value[1] :
+                    for i in range(-1,2,2) :
+                        x,y = key
+                        if not my_map.get((x+i, y)) :
+                            if tempdic.get((x+i, y)) :
+                                if value[0] > tempdic.get((x+i, y))[0] :
+                                    tempdic.update({(x+i,y) : (value[0], value[0]*2)})
+                            else :
+                                tempdic.update({(x+i,y) : (value[0], value[0]*2)})    
+                        if not my_map.get((x, y+i)) :
+                            if tempdic.get((x, y+i)) :
+                                if value[0] > tempdic.get((x, y+i))[0] :
+                                    tempdic.update({(x,y+i) : (value[0], value[0]*2)})
+                            else :
+                                tempdic.update({(x,y+i) : (value[0], value[0]*2)})
+        for key,value in my_map.items() :
+            my_map[key] = (value[0],value[1]-1)
+        if tempdic :
+            my_map.update(tempdic)
+        time += 1
+    for key,value in my_map.items() :
+        if value[0] > 0 and value[1] > 0 :
+            result += 1
+    print(f'#{a+1} {result}')
