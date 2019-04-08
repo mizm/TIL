ce8c84525df717c353f5575ed5233b3178878a24
miz <didehgur1@naver.com>
2019-04-08 09:15:10 +0900

    190408 git test

--- algorithm/Day34/sw4311오래된.py
+++ algorithm/Day34/sw4311오래된.py
@@ -1,14 +1,9 @@
 for tc in range(int(input())) :
     n,o,m = list(map(int,input().split()))
     item = list(map(int,input().split()))
-    item.sort()
-    temp = list(map(int,input().split()))
+    oper = list(map(int,input().split()))
     w = int(input())
-    opcode = [-1,-2,-3,-4]
-    oper = []
-    for i in temp :
-        oper.append(opcode[i-1])
-    c = [0] * 1000
+    c = [1000] * 1000
     ar = []
     for i in item :
         if i == 0 :
@@ -16,17 +11,17 @@ for tc in range(int(input())) :
             continue
         for j in item :
             for k in item :
-                if not c[i]:
-                    c[i] = 1
-                    ar.append([i, 1])
-                if not c[i*10+j] :
-                    c[i*10+j] = 2
-                    ar.append([i * 10 + j, 2])
-                if not c[i*100+j*10+k] and i*100+j*10+k < 1000:
-                    c[i*100+j*10+k] = 3
-                    ar.append([i*100+j*10+k, 3])
-    res = 987654321
-    if c[w] :
+                # if not c[i]:
+                c[i] = 1
+                ar.append((i, 1))
+                # if not c[i*10+j] :
+                c[i*10+j] = 2
+                ar.append((i * 10 + j, 2))
+                # if not c[i*100+j*10+k] and i*100+j*10+k < 1000:
+                c[i*100+j*10+k] = 3
+                ar.append((i*100+j*10+k, 3))
+    res = -1
+    if c[w] != 1000:
         res = c[w]
     else :
         Q = []
@@ -34,27 +29,25 @@ for tc in range(int(input())) :
             Q.append(i)
         while Q :
             a,b = Q.pop(0)
-            # print(a,b)
-            if a == w :
-                if res > b :
-                    res = b
-                continue
             for i in ar :
-                c,d = i
-                if b+d+2 > m or b+d+2 >= res:
+                e,d = i
+                cnt = b+d+1
+                if cnt >= m:
                     continue
                 for j in range(o) :
-                    if oper[j] == -1 :
-                        k = a+c
-                    if oper[j] == -2 :
-                        k = a-c
-                    if oper[j] == -3 :
-                        k = a*c
-                    if oper[j] == -4 :
-                        if c == 0 :
-                            continue
-                        k = a//c
-                    if 0<=k<=999 :
-                        Q.append([k,b+d+2])
-                        ar.append([k,b+d+2])
-    print(res)
+                    k= 0
+                    if oper[j] == 1 :
+                        k = a+e
+                    if oper[j] == 2 :
+                        k = a-e
+                    if oper[j] == 3 :
+                        k = a*e
+                    if oper[j] == 4 :
+                        k = a//e
+                    if 0 >  k or k > 999 or c[k] <= cnt :
+                    	continue
+                    Q.append((k,cnt))
+                    c[k] = cnt
+        if c[w] > 0 and c[w] != 1000 :
+            res = c[w]+1
+    print("#%d %d"%(tc+1,res))
